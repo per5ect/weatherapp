d3fa96c5571b0929d54579f5e0d7a180f5488757
per5ecct <anikolajcuk717@gmail.com>
2025-02-15 22:58:25 +0100

    update api settings

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,5 @@
-export const API_URL = import.meta.env.VITE_WEATHER_API_URL;
-export const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+export const VITE_WEATHER_API_URL = import.meta.env.VITE_WEATHER_API_URL;
+export const VITE_WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 export const CNT_COUNT = 5;
 export const UNITS = "metric"
--- src/imageApiService.js
+++ src/imageApiService.js
@@ -1,19 +1,18 @@
 import { baseFetch } from "./helpers/baseFetch.js";
 
-
 function createImageApiService(){
-    const IMAGE_API_URL = import.meta.env.VITE_IMAGE_API_URL;
-    const IMAGE_ACCESS_KEY = import.meta.env.VITE_IMAGE_API_ACCESS_KEY;
+    const VITE_IMAGE_API_URL = import.meta.env.VITE_IMAGE_API_URL;
+    const VITE_IMAGE_API_ACCESS_KEY = import.meta.env.VITE_IMAGE_API_ACCESS_KEY;
 
     const options = {
         headers: {
             "Accept-Version": "v1",
-            "Authorization": `Client-ID ${IMAGE_ACCESS_KEY}`
+            "Authorization": `Client-ID ${VITE_IMAGE_API_ACCESS_KEY}`
         }
     }
 
     function fetchImageByKeywords(keywords){
-        return baseFetch(`${IMAGE_API_URL}?query=${keywords}&client_id=${IMAGE_ACCESS_KEY}`, options)
+        return baseFetch(`${VITE_IMAGE_API_URL}?query=${keywords}&client_id=${VITE_IMAGE_API_ACCESS_KEY}`, options)
     }
 
     return{
